fb296052f0ee7645c3b0d9488cc6db6b368fc867
Daniel Bachar <danielbac@zillowgroup.com>
2024-03-06 18:19:40 -0700

    Commit the issue

---
+++ SPM-Wraps-XCFramework-With-BinaryTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM-Wraps-XCFramework-With-BinaryTarget
+//
+//  Created by Daniel Bachar on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
